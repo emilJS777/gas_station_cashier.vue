3fddfa7e58025cf51a29824665b770990ac96d38
emil <e.pargevich@mail.ru>
2022-05-11 18:37:29 +0400

    asdds

--- src/store/modules/_request.js
+++ src/store/modules/_request.js
@@ -1,8 +1,8 @@
 import Axios from "axios";
 import store from "@/store";
 
-const api_url = "http://localhost:5001/api"
-// const api_url = "http://37.252.64.153:5001/api"
+// const api_url = "http://localhost:5001/api"
+const api_url = "http://37.252.64.153:5001/api"
 // const api_url = "http://192.168.0.10:5001/api"
 
 const REFRESH = (payload) => {
